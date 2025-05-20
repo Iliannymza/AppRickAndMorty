2af25cc8a2f64d2dc8eb85c3d45cc3a05ab45a3a
Iliannymza <mendoza71997@gmail.com>
2025-05-20 18:09:46 +0200

    Comienzo al proyecto, se dio el acceso a internet y se comienza a abrir los package que se estaran usando.

---
+++ app/build.gradle.kts
@@ -0,0 +1,64 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.example.apprickandmorty"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.example.apprickandmorty"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+    // se agrega para llamar al binding
+    buildFeatures {
+        viewBinding = true
+    }
+
+    // esta tambien se agrega
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    // Retrofit
+    val retrofitVersion = "2.11.0"
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation ("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    //Picasso
+    implementation("com.squareup.picasso:picasso:2.8")
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/example/apprickandmorty/activities/MainActivity.kt
@@ -0,0 +1,4 @@
+package com.example.apprickandmorty.activities
+
+class MainActivity {
+}
